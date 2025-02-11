69661de25c0edcb0766d3a2133f8a033ed045e97
Justin Lee <justin.adam.lee@gmail.com>
2025-02-11 15:41:58 -0500

    add 13

---
+++ src/bin/13.roman-to-integer.rs
@@ -0,0 +1,32 @@
+impl Solution {
+    pub fn roman_to_int(mut s: String) -> i32 {
+        let values_and_symbols = [
+            (900, "CM"),
+            (400, "CD"),
+            (90, "XC"),
+            (40, "XL"),
+            (9, "IX"),
+            (4, "IV"),
+            (1000, "M"),
+            (500, "D"),
+            (100, "C"),
+            (50, "L"),
+            (10, "X"),
+            (5, "V"),
+            (1, "I")
+        ];
+        let mut num = 0;
+
+        while !s.is_empty() {
+            for (value, symbol) in values_and_symbols {
+                if s.starts_with(symbol) {
+                    s = s.strip_prefix(symbol).unwrap().to_string();
+                    num += value;
+                    break;
+                }
+            }
+        }
+
+        num
+    }
+}
