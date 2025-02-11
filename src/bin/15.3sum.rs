0f1e119967a510d5b290d3599ad0ed792f7a42d6
Justin Lee <justin.adam.lee@gmail.com>
2025-02-11 18:15:26 -0500

    3sum

---
+++ src/bin/15.3sum.rs
@@ -0,0 +1,36 @@
+use std::collections::{HashMap, HashSet};
+
+impl Solution {
+    pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+        nums.sort();
+
+        let mut triplets = Vec::new();
+
+        for i in 0..nums.len() {
+            if i > 0 && nums[i] == nums[i - 1] {
+                continue;
+            }
+
+            let mut left_ptr = i + 1;
+            let mut right_ptr = nums.len() - 1;
+
+            while left_ptr < right_ptr {
+                let sum = nums[i] + nums[left_ptr] + nums[right_ptr];
+
+                if (sum > 0) {
+                    right_ptr -= 1;
+                } else if (sum < 0) {
+                    left_ptr += 1;
+                } else {
+                    triplets.push(vec![nums[i], nums[left_ptr], nums[right_ptr]]);
+
+                    while left_ptr < right_ptr && nums[i] + nums[left_ptr] + nums[right_ptr] == 0 {
+                        left_ptr += 1;
+                    }
+                }
+            }
+        }
+
+        triplets
+    }
+}
