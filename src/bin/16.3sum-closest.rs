bb8166f4a14c87c2697bfb11d12051ac2e5e95dd
Justin Lee <justin.adam.lee@gmail.com>
2025-02-11 18:24:38 -0500

    little bit better

--- src/bin/16.3sum-closest.rs
+++ src/bin/16.3sum-closest.rs
@@ -2,7 +2,7 @@ impl Solution {
     pub fn three_sum_closest(mut nums: Vec<i32>, target: i32) -> i32 {
         nums.sort();
 
-        let mut closest_target = i32::MAX;
+        let mut closest_dist = i32::MAX;
         let mut closest_sum = 0;
 
         for i in 0..nums.len() {
@@ -15,16 +15,19 @@ impl Solution {
 
             while left_ptr < right_ptr {
                 let sum = nums[i] + nums[left_ptr] + nums[right_ptr];
+                let dist = (sum - target).abs();
 
-                if (sum - target).abs() < closest_target {
-                    closest_target = (sum - target).abs();
+                if dist < closest_dist {
+                    closest_dist = dist;
                     closest_sum = sum;
                 }
 
                 if (sum > target) {
                     right_ptr -= 1;
-                } else {
+                } else if (sum < target) {
                     left_ptr += 1;
+                } else {
+                    return target;
                 }
             }
         }
