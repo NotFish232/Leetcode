fb26ac0125ecab9c74e9946c0e279cb523daf392
Justin Lee <justin.adam.lee@gmail.com>
2025-02-11 15:49:32 -0500

    more spacing

--- src/bin/1910.remove-all-occurrences-of-a-substring.rs
+++ src/bin/1910.remove-all-occurrences-of-a-substring.rs
@@ -1,6 +1,7 @@
 impl Solution {
     pub fn remove_occurrences(s: String, part: String) -> String {
         let mut s_copy = s.to_string();
+        
         loop {
             if let Some(idx) = s_copy.find(part.as_str()) {
                 for _ in 0..part.len() {
