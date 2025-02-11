f896cd665e3c8d9eaa19be6cecedf53804fa5cdf
Justin Lee <justin.adam.lee@gmail.com>
2025-02-11 18:43:32 -0500

    17

---
+++ src/bin/17.letter-combinations-of-a-phone-number.rs
@@ -0,0 +1,41 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn letter_combinations(digits: String) -> Vec<String> {
+        if digits.len() == 0 {
+            return Vec::new();
+        }
+
+        let digit_to_letters = HashMap::from([
+            ("2", "abc"),
+            ("3", "def"),
+            ("4", "ghi"),
+            ("5", "jkl"),
+            ("6", "mno"),
+            ("7", "pqrs"),
+            ("8", "tuv"),
+            ("9", "wxyz"),
+        ]);
+
+        let letters = digit_to_letters.get(&digits.get(0..1).unwrap()).unwrap();
+
+        if digits.len() == 1 {
+            return letters.chars().map(|c| c.to_string()).collect();
+        }
+
+        let mut combinations = Vec::new();
+
+        let future_combinations =
+            Solution::letter_combinations(digits.get(1..digits.len()).unwrap().to_string());
+
+        for letter in letters.chars() {
+            for future_combo in &future_combinations {
+                let mut owned_letter = letter.to_string();
+                owned_letter.push_str(future_combo.as_str());
+                combinations.push(owned_letter);
+            }
+        }
+
+        combinations
+    }
+}
