aeca25b069bd5eed62a02e01b7b45727149e067c
Justin Lee <justin.adam.lee@gmail.com>
2025-02-13 10:20:59 -0500

    323 and 33

---
+++ src/bin/32.longest-valid-parentheses.rs
@@ -0,0 +1,36 @@
+use std::cmp::max;
+
+impl Solution {
+    pub fn longest_valid_parentheses(s: String) -> i32 {
+        let mut v = vec![0; s.len() + 1];
+
+        let mut stack = Vec::new();
+
+        for (i, c) in s.chars().enumerate() {
+            if c == '(' {
+                stack.push((c, i))
+            } else {
+                if let Some(x) = stack.pop() {
+                    if x.0 == '(' {
+                        v[i] = 1;
+                        v[x.1] = 1;
+                    }
+                }
+            }
+        }
+
+        let mut cur_len = 0;
+        let mut max_len = 0;
+
+        for x in v {
+            if x == 1 {
+                cur_len += 1;
+            } else {
+                max_len = max(max_len, cur_len);
+                cur_len = 0;
+            }
+        }
+
+        max_len
+    }
+}
---
+++ src/bin/33.search-in-rotated-sorted-array.rs
@@ -0,0 +1,34 @@
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        let mut left_ptr = 0;
+        let mut right_ptr = nums.len() - 1;
+
+        while left_ptr <= right_ptr {
+            let mid_ptr = (left_ptr + right_ptr) / 2;
+
+            let left_val = nums[left_ptr];
+            let right_val = nums[right_ptr];
+            let mid_val = nums[mid_ptr];
+
+            if mid_val == target {
+                return mid_ptr as i32;
+            }
+
+            if nums[left_ptr] <= nums[mid_ptr] {
+                if nums[left_ptr] <= target && target < nums[mid_ptr] {
+                    right_ptr = mid_ptr - 1;
+                } else {
+                    left_ptr = mid_ptr + 1;
+                }
+            } else {
+                if nums[mid_ptr] < target && target <= nums[right_ptr] {
+                    left_ptr = mid_ptr + 1;
+                } else {
+                    right_ptr = mid_ptr - 1;
+                }
+            }
+        }
+
+        -1
+    }
+}
