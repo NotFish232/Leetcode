931c89270f7fe45e69cb96fcb36b7892f8601d21
Justin Lee <justin.adam.lee@gmail.com>
2025-02-14 09:36:24 -0500

    finish 1352

---
+++ src/bin/1352.product-of-the-last-k-numbers.rs
@@ -0,0 +1,37 @@
+struct ProductOfNumbers {
+    v: Vec<i32>,
+    prod: i32,
+}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl ProductOfNumbers {
+    fn new() -> Self {
+        ProductOfNumbers {
+            v: Vec::new(),
+            prod: 1,
+        }
+    }
+
+    fn add(&mut self, num: i32) {
+        if num == 0 {
+            self.v.clear();
+            self.prod = 1;
+        } else {
+            self.prod *= num;
+            self.v.push(self.prod);
+        }
+    }
+
+    fn get_product(&self, k: i32) -> i32 {
+        if self.v.len() < k as usize {
+            0
+        } else if self.v.len() == k as usize {
+            self.v[self.v.len() - 1]
+        } else {
+            self.v[self.v.len() - 1] / self.v[self.v.len() - 1 - k as usize]
+        }
+    }
+}
