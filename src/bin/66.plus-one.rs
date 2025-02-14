8bde90de6ce168dfb2e22c0e2e822047a4ff20af
Justin Lee <justin.adam.lee@gmail.com>
2025-02-14 13:46:59 -0500

    add 66

---
+++ src/bin/66.plus-one.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn plus_one(mut digits: Vec<i32>) -> Vec<i32> {
+        let n = digits.len();
+
+        digits.insert(0, 0);
+        digits[n] += 1;
+
+        for i in (0..n).rev() {
+            digits[i] += digits[i + 1] / 10;
+            digits[i + 1] %= 10;
+        }
+
+        if digits.first() == Some(&0) {
+            digits.remove(0);
+        }
+
+        digits
+    }
+}
