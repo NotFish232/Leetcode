160b4b999855320693113b9a9615b497848a20de
Justin Lee <justin.adam.lee@gmail.com>
2025-02-17 12:21:07 -0500

    139

---
+++ src/bin/139.word-break.rs
@@ -0,0 +1,33 @@
+use std::collections::HashSet;
+
+impl Solution {
+    fn _word_break(s: String, word_dict: &Vec<String>, memo: &mut HashSet<String>) -> bool {
+        if memo.contains(&s) {
+            return false;
+        }
+
+        if s.is_empty() {
+            return true;
+        }
+
+        for i in 0..word_dict.len() {
+            if s.starts_with(&word_dict[i]) {
+                let new_s = s.strip_prefix(&word_dict[i]).unwrap().to_string();
+
+                if Solution::_word_break(new_s, word_dict, memo) {
+                    return true;
+                }
+            }
+        }
+
+        memo.insert(s);
+
+        false
+    }
+
+    pub fn word_break(s: String, mut word_dict: Vec<String>) -> bool {
+        let mut memo = HashSet::new();
+
+        Solution::_word_break(s, &word_dict, &mut memo)
+    }
+}
