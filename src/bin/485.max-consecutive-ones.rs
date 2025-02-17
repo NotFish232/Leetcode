3a7e8d0141a9747c65497f7f69e98faf79d2a7f8
Justin Lee <justin.adam.lee@gmail.com>
2025-02-17 16:00:08 -0500

    485

---
+++ src/bin/485.max-consecutive-ones.rs
@@ -0,0 +1,19 @@
+use std::cmp::max;
+
+impl Solution {
+    pub fn find_max_consecutive_ones(nums: Vec<i32>) -> i32 {
+        let mut max_count = 0;
+        let mut cur_count = 0;
+
+        for n in nums.into_iter().chain([0]) {
+            if n == 1 {
+                cur_count += 1;
+            } else {
+                max_count = max(max_count, cur_count);
+                cur_count = 0;
+            }
+        }
+
+        max_count as i32
+    }
+}
