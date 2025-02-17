9eba7f076a8740560a62ad96a1c03d2f8063a6a8
Justin Lee <justin.adam.lee@gmail.com>
2025-02-16 23:44:22 -0500

    collect is better

--- src/bin/3271.hash-divided-string.rs
+++ src/bin/3271.hash-divided-string.rs
@@ -1,8 +1,12 @@
 impl Solution {
     pub fn string_hash(s: String, k: i32) -> String {
-        String::from_iter(s.chars().collect::<Vec<_>>().chunks(k as usize).map(|x| {
-            ('a' as u128 + (x.iter().map(|&c| c as u128 - 'a' as u128).sum::<u128>() % 26)) as u8
-                as char
-        }))
+        s.chars()
+            .collect::<Vec<_>>()
+            .chunks(k as usize)
+            .map(|x| {
+                ('a' as u8 + (x.iter().map(|&c| c as u16 - 'a' as u16).sum::<u16>() % 26) as u8)
+                    as char
+            })
+            .collect()
     }
 }
