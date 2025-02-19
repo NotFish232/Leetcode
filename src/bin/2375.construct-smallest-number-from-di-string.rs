3a3988ff17009e6f1a65999d5b351494995d9216
Justin Lee <justin.adam.lee@gmail.com>
2025-02-18 22:06:10 -0500

    done

---
+++ src/bin/2375.construct-smallest-number-from-di-string.rs
@@ -0,0 +1,44 @@
+impl Solution {
+    fn gen_number_patterns(
+        current: &mut String,
+        chars: &mut Vec<char>,
+        pattern: &String,
+    ) -> Option<String> {
+        if current.len() == pattern.len() + 1 {
+            return Some(current.to_string());
+        }
+
+        let i = current.len();
+        for j in 0..chars.len() {
+            let ch = chars[j];
+
+            if i == 0
+                || (&pattern[i - 1..i] == "I" && ch > current[i - 1..i].chars().next().unwrap())
+                || (&pattern[i - 1..i] == "D" && ch < current[i - 1..i].chars().next().unwrap())
+            {
+                current.push(ch);
+                chars.remove(j);
+
+                let res = Solution::gen_number_patterns(current, chars, pattern);
+
+                if res.is_some() {
+                    return res;
+                }
+
+                current.pop();
+                chars.insert(j, ch);
+            }
+        }
+
+        None
+    }
+
+    pub fn smallest_number(pattern: String) -> String {
+
+        Solution::gen_number_patterns(
+            &mut String::new(),
+            &mut "123456789".chars().collect(),
+            &pattern,
+        ).unwrap()
+    }
+}
