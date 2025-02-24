a27f1a5978e73f9e9cc10367213d4d24c59c5862
Justin Lee <justin.adam.lee@gmail.com>
2025-02-24 09:03:34 -0500

    2068

---
+++ src/bin/2068.check-whether-two-strings-are-almost-equivalent.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn check_almost_equivalent(word1: String, word2: String) -> bool {
+        word1
+            .chars()
+            .zip(word2.chars())
+            .fold([0i32; 26], |mut v, (ch1, ch2)| {
+                v[ch1 as usize - 'a' as usize] += 1;
+                v[ch2 as usize - 'a' as usize] -= 1;
+                v
+            })
+            .into_iter()
+            .all(|x| x.abs() <= 3)
+    }
+}
