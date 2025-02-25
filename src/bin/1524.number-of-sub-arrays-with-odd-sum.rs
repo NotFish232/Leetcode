13c62f1fd6fe3224eef8e86c612b4659e7abaed8
Justin Lee <justin.adam.lee@gmail.com>
2025-02-25 09:42:46 -0500

    daily

---
+++ src/bin/1524.number-of-sub-arrays-with-odd-sum.rs
@@ -0,0 +1,24 @@
+impl Solution {
+    pub fn num_of_subarrays(mut arr: Vec<i32>) -> i32 {
+        for i in 1..arr.len() {
+            arr[i] = (arr[i] + arr[i - 1]) % 2;
+        }
+
+        let mut num_even_sums = 1;
+        let mut num_odd_sums = 0;
+        let mut count = 0;
+        let modulo = 10i32.pow(9) + 7;
+
+        for num in arr {
+            if num % 2 == 0 {
+                count = (count + num_odd_sums) % modulo;
+                num_even_sums += 1;
+            } else {
+                count = (count + num_even_sums) % modulo;
+                num_odd_sums += 1;
+            }
+        }
+
+        count
+    }
+}
