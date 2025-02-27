39c6f18383ddab746f9958f457a6f282931c813b
Justin Lee <justin.adam.lee@gmail.com>
2025-02-27 13:55:40 -0500

    thing

---
+++ src/bin/1665.minimum-initial-energy-to-finish-tasks.rs
@@ -0,0 +1,15 @@
+use std::cmp::max;
+
+impl Solution {
+    pub fn minimum_effort(mut tasks: Vec<Vec<i32>>) -> i32 {
+        tasks.sort_by(|a, b| (a[1] - a[0]).cmp(&(b[1] - b[0])));
+
+        let mut count = 0;
+
+        for x in tasks {
+            count = x[1].max(x[0] + count);
+        }
+
+        count
+    }
+}
