60c515bc9fb59cea528777afa63c3af643473cc0
Justin Lee <justin.adam.lee@gmail.com>
2025-02-27 09:26:43 -0500

    dumb question

---
+++ src/bin/3207.maximum-points-after-enemy-battles.rs
@@ -0,0 +1,28 @@
+impl Solution {
+    pub fn maximum_points(mut enemy_energies: Vec<i32>, mut current_energy: i32) -> i64 {
+        let (min_idx, min_val) = enemy_energies
+            .iter()
+            .enumerate()
+            .min_by(|(_, a), (_, b)| a.cmp(b))
+            .map(|(i, &x)| (i, x))
+            .unwrap();
+
+        if current_energy < min_val {
+            return 0;
+        }
+
+        let mut count = (current_energy / min_val) as i64;
+        current_energy %= min_val;
+
+        for (i, e) in enemy_energies.into_iter().enumerate() {
+            if i == min_idx {
+                continue;
+            }
+            current_energy += e;
+            count += (current_energy / min_val) as i64;
+            current_energy %= min_val;
+        }
+
+        count
+    }
+}
