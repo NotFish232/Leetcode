d2ab6759a3e9acc3af121ae18bdb069a4707d478
Justin Lee <justin.adam.lee@gmail.com>
2025-02-27 08:53:09 -0500

    daily

---
+++ src/bin/873.length-of-longest-fibonacci-subsequence.rs
@@ -0,0 +1,26 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn len_longest_fib_subseq(arr: Vec<i32>) -> i32 {
+        let mut dp = vec![vec![2; arr.len()]; arr.len()];
+        let mut val_idxs: HashMap<_, usize> = HashMap::new();
+        let mut longest_subseq = i32::MIN;
+
+        for i in 0..arr.len() {
+            for j in 0..i {
+                let prev_seq_val = arr[i] - arr[j];
+
+                if prev_seq_val < arr[j] && val_idxs.contains_key(&prev_seq_val) {
+                    dp[j][i] = dp[j][i].max(dp[*val_idxs.get(&prev_seq_val).unwrap()][j] + 1);
+
+                longest_subseq = longest_subseq.max(dp[j][i]);
+                }
+            }
+
+            val_idxs.insert(arr[i], i);
+        }
+
+        longest_subseq.max(0)
+        
+    }
+}
