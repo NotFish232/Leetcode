c9dec7259433f4d2f9cf54df6a6fc074a1165104
Justin Lee <justin.adam.lee@gmail.com>
2025-02-28 20:09:00 -0500

    one pass

--- src/bin/2460.apply-operations-to-an-array.rs
+++ src/bin/2460.apply-operations-to-an-array.rs
@@ -1,25 +1,20 @@
 impl Solution {
     pub fn apply_operations(mut nums: Vec<i32>) -> Vec<i32> {
-        let mut i = 0;
-        let mut num_zeros = 0;
-        while i < nums.len() {
+        let mut v = vec![0; nums.len()];
+
+        let mut v_i = 0;
+        for i in 0..nums.len() {
             if i + 1 < nums.len() && nums[i] == nums[i + 1] {
                 nums[i] *= 2;
                 nums[i + 1] = 0;
             }
-            if nums[i] == 0 {
-                num_zeros += 1;
-                nums.remove(i);
-                i -= 1;
+
+            if nums[i] != 0 {
+                v[v_i] = nums[i];
+                v_i += 1;
             }
-
-            i += 1;
         }
 
-        for _ in 0..num_zeros {
-            nums.push(0);
-        }
-
-        nums
+        v
     }
 }
