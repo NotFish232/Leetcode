a48e2e95f20b4e079b2380324a83310c8d61f735
Justin Lee <justin.adam.lee@gmail.com>
2025-02-28 22:13:49 -0500

    another hard

---
+++ src/bin/3273.minimum-amount-of-damage-dealt-to-bob.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn min_damage(power: i32, damage: Vec<i32>, health: Vec<i32>) -> i64 {
+        let mut v: Vec<_> = health.into_iter().zip(damage.into_iter()).collect();
+        v.sort_by(|(h1, d1), (h2, d2)| {
+            ((h1 + power - 1) / power * d2).cmp(&((h2 + power - 1) / power * d1))
+        });
+
+        let mut t = 0;
+        let mut c = 0i64;
+
+        for (h, d) in v {
+            t += (h + power - 1) / power;
+            c += t as i64 * d as i64;
+        }
+
+        c
+    }
+}
