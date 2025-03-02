42deb51724af96014b797ec930f0c104c39dcf05
Justin Lee <justin.adam.lee@gmail.com>
2025-03-01 21:11:12 -0500

    this

---
+++ src/bin/213.house-robber-ii.rs
@@ -0,0 +1,25 @@
+use std::cmp::max;
+
+impl Solution {
+    pub fn rob(nums: Vec<i32>) -> i32 {
+        if nums.len() == 1 {
+            return nums[0];
+        }
+
+        let mut dp1 = vec![0; nums.len()];
+        dp1[1] = nums[0];
+
+        for i in 1..nums.len() - 1 {
+            dp1[i + 1] = max(dp1[i - 1] + nums[i], dp1[i]);
+        }
+
+        let mut dp2 = vec![0; nums.len()];
+        dp2[nums.len() - 2] = nums[nums.len() - 1];
+
+        for i in (1..nums.len() - 1).rev() {
+            dp2[i - 1] = max(dp2[i + 1] + nums[i], dp2[i]);
+        }
+
+        max(dp1[dp1.len() - 1], dp2[0])
+    }
+}
---
+++ src/bin/3410.maximize-subarray-sum-after-removing-all-occurrences-of-one-element.rs
@@ -0,0 +1,3 @@
+impl Solution {
+    pub fn max_subarray_sum(nums: Vec<i32>) -> i64 {}
+}
