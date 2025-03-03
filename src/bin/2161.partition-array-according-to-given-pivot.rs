67f9946a40b9893a64d7db524af7a41aca1ecf81
Justin Lee <justin.adam.lee@gmail.com>
2025-03-03 12:58:34 -0500

    daiily

---
+++ src/bin/2161.partition-array-according-to-given-pivot.rs
@@ -0,0 +1,22 @@
+impl Solution {
+    pub fn pivot_array(nums: Vec<i32>, pivot: i32) -> Vec<i32> {
+        let mut prev = Vec::new();
+        let mut post = Vec::new();
+        let mut pivot_count = 0;
+
+        for num in nums {
+            if num < pivot {
+                prev.push(num);
+            } else if num > pivot {
+                post.push(num);
+            } else {
+                pivot_count += 1;
+            }
+        }
+
+        prev.extend(vec![pivot; pivot_count]);
+        prev.extend(post);
+
+        prev
+    }
+}
