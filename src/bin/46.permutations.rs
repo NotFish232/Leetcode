df66dcd96ab4fcfe5d2e0c91311ce1b54c1ddc34
Justin Lee <justin.adam.lee@gmail.com>
2025-03-03 19:49:14 -0500

    46

---
+++ src/bin/46.permutations.rs
@@ -0,0 +1,30 @@
+impl Solution {
+    fn _create_permutations(
+        current: &mut Vec<i32>,
+        nums: &mut Vec<i32>,
+        result: &mut Vec<Vec<i32>>,
+    ) {
+        if nums.is_empty() {
+            result.push(current.clone());
+            return;
+        }
+
+        for i in 0..nums.len() {
+            let num = nums.remove(i);
+            current.push(num);
+
+            Self::_create_permutations(current, nums, result);
+
+            nums.insert(i, num);
+            current.pop();
+        }
+    }
+
+    pub fn permute(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut result = Vec::new();
+
+        Self::_create_permutations(&mut Vec::new(), &mut nums, &mut result);
+
+        result
+    }
+}
