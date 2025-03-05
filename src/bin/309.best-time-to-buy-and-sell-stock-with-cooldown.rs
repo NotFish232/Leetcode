9776949247f85830d9f2d1d2c6a43de3748b8d68
Justin Lee <justin.adam.lee@gmail.com>
2025-03-05 18:32:02 -0500

    dp

---
+++ src/bin/309.best-time-to-buy-and-sell-stock-with-cooldown.rs
@@ -0,0 +1,17 @@
+use std::cmp::max;
+
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut dp = vec![vec![0; 3]; prices.len()];
+        dp[0][0] = -prices[0];
+        // 0 -> buy, 1 -> sell, 2 -> cooldown
+
+        for i in 1..prices.len() {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][2] - prices[i]);
+            dp[i][1] = dp[i - 1][0] + prices[i];
+            dp[i][2] = max(dp[i - 1][2], dp[i - 1][1]);
+        }
+
+        max(dp[prices.len() - 1][1], dp[prices.len() - 1][2])
+    }
+}
