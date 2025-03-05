6af5008acd4bc841094c9b3208180a28ec76d424
Justin Lee <justin.adam.lee@gmail.com>
2025-03-05 18:50:13 -0500

    121

---
+++ src/bin/121.best-time-to-buy-and-sell-stock.rs
@@ -0,0 +1,15 @@
+use std::cmp::max;
+
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut dp = vec![vec![0; 2]; prices.len()];
+        dp[0][0] = -prices[0];
+
+        for i in 1..prices.len() {
+            dp[i][0] = max(dp[i - 1][0], -prices[i]);
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        }
+
+        dp[prices.len() - 1][1]
+    }
+}
