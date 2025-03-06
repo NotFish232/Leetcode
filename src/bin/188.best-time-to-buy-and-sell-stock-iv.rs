ef680822798e58cdf2418b4bf49fe952ff633e93
Justin Lee <justin.adam.lee@gmail.com>
2025-03-05 19:51:27 -0500

    whoops

--- src/bin/188.best-time-to-buy-and-sell-stock-iv.rs
+++ src/bin/188.best-time-to-buy-and-sell-stock-iv.rs
@@ -2,20 +2,20 @@ use std::cmp::max;
 
 impl Solution {
     pub fn max_profit(k: i32, prices: Vec<i32>) -> i32 {
-        let mut sells = vec![-prices[0]; k as usize];
-        let mut buys = vec![0; k as usize];
+        let mut buys = vec![-prices[0]; k as usize];
+        let mut sells = vec![0; k as usize];
 
         for i in 1..prices.len() {
             for j in 0..k as usize {
                 if j == 0 {
-                    sells[j] = max(sells[j], -prices[i]);
+                    buys[j] = max(buys[j], -prices[i]);
                 } else {
-                    sells[j] = max(sells[j], buys[j - 1] - prices[i]);
+                    buys[j] = max(buys[j], sells[j - 1] - prices[i]);
                 }
-                buys[j] = max(buys[j], sells[j] + prices[i]);
+                sells[j] = max(sells[j], buys[j] + prices[i]);
             }
         }
 
-        buys.into_iter().max().unwrap()
+        sells.into_iter().max().unwrap()
     }
 }
