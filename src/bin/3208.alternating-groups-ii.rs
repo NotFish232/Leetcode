5e0fae121c08f8073b146137ca1d97e9ff5ebb0b
Justin Lee <justin.adam.lee@gmail.com>
2025-03-09 05:13:02 -0400

    daily

---
+++ src/bin/3208.alternating-groups-ii.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn number_of_alternating_groups(colors: Vec<i32>, k: i32) -> i32 {
+        let mut cur_len = 1;
+        let mut count = 0;
+        
+        for i in 1..colors.len() + k as usize - 1 {
+            if colors[i % colors.len()] == colors[(i - 1) % colors.len()] {
+                cur_len = 1;
+            } else {
+                cur_len += 1;
+                if cur_len == k {
+                    count += 1;
+                    cur_len -= 1;
+                }
+            }
+        }
+
+        count as i32
+    }
+}
