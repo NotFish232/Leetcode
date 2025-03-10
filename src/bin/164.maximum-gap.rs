6a4a80642f22521071cfe016811e2299b582bd0a
Justin Lee <justin.adam.lee@gmail.com>
2025-03-10 11:20:28 -0400

    164

---
+++ src/bin/164.maximum-gap.rs
@@ -0,0 +1,15 @@
+use std::cmp::max;
+
+impl Solution {
+    pub fn maximum_gap(mut nums: Vec<i32>) -> i32 {
+        nums.sort();
+
+        let mut max_gap = 0;
+
+        for i in 1..nums.len() {
+            max_gap = max(max_gap, nums[i] - nums[i - 1])
+        }
+
+        max_gap
+    }
+}
