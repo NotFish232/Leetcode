bf58b25dd7f4bd6d4a8b4c6c4cb5916ada32f45e
Justin Lee <justin.adam.lee@gmail.com>
2025-03-10 21:25:00 -0400

    didnt read the question properly

--- src/bin/1358.number-of-substrings-containing-all-three-characters.rs
+++ src/bin/1358.number-of-substrings-containing-all-three-characters.rs
@@ -10,16 +10,12 @@ impl Solution {
         let mut count = 0;
 
         while right_ptr < s.len() {
-            if let ch @ (b'a' | b'b' | b'c') = s.as_bytes()[right_ptr] {
-                seen[(ch - b'a') as usize] += 1
-            }
+            seen[(s.as_bytes()[right_ptr] - b'a') as usize] += 1;
 
             while seen[0] > 0 && seen[1] > 0 && seen[2] > 0 {
                 count += (s.len() - right_ptr) as i32;
 
-                if let ch @ (b'a' | b'b' | b'c') = s.as_bytes()[left_ptr] {
-                    seen[(ch - b'a') as usize] -= 1
-                }
+                seen[(s.as_bytes()[left_ptr] - b'a') as usize] -= 1;
 
                 left_ptr += 1;
             }
