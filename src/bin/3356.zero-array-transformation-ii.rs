22b2eb5e0188bfd494a2fc5e0c578ac2aa56b2ad
Justin Lee <justin.adam.lee@gmail.com>
2025-03-13 14:15:41 -0400

    hard

---
+++ src/bin/3356.zero-array-transformation-ii.rs
@@ -0,0 +1,33 @@
+impl Solution {
+    pub fn min_zero_array(mut nums: Vec<i32>, queries: Vec<Vec<i32>>) -> i32 {
+        let partition_point_func = |&i: &usize| -> bool {
+            let mut v = vec![0; nums.len() + 1];
+
+            for q in queries.iter().take(i) {
+                v[q[0] as usize] += q[2];
+                v[q[1] as usize + 1] -= q[2];
+            }
+
+            let mut p_sum = 0;
+            for j in 0..nums.len() {
+                p_sum += v[j];
+
+                if p_sum < nums[j] {
+                    return true;
+                }
+            }
+
+            false
+        };
+
+        let idxs: Vec<_> = (0..=queries.len()).collect();
+
+        let point = idxs.partition_point(partition_point_func);
+
+        if point != queries.len() + 1 {
+            point as i32
+        } else {
+            -1
+        }
+    }
+}
