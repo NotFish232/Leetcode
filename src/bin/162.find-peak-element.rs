d13c17f93855f41d5e964ff3e35c7180546ca0cf
Justin Lee <justin.adam.lee@gmail.com>
2025-03-13 14:47:52 -0400

    162

---
+++ src/bin/162.find-peak-element.rs
@@ -0,0 +1,23 @@
+use std::cmp::{self, Ordering};
+
+impl Solution {
+    pub fn find_peak_element(nums: Vec<i32>) -> i32 {
+        let search_fn = |&i: &usize| -> cmp::Ordering {
+            let num = nums[i];
+            let lhs = *nums.get(i - 1).unwrap_or(&i32::MIN);
+            let rhs = *nums.get(i + 1).unwrap_or(&i32::MIN);
+
+            if num >= lhs && num >= rhs {
+                Ordering::Equal
+            } else if lhs >= num {
+                Ordering::Greater
+            } else {
+                Ordering::Less
+            }
+        };
+
+        let idxs: Vec<_> = (0..nums.len()).collect();
+
+        idxs.binary_search_by(search_fn).unwrap() as i32
+    }
+}
