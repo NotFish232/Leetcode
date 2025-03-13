9d893d0b7465cb1ac612fc069de6743cd7b8fa01
Justin Lee <justin.adam.lee@gmail.com>
2025-03-13 14:57:40 -0400

    1109

---
+++ src/bin/1109.corporate-flight-bookings.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn corp_flight_bookings(bookings: Vec<Vec<i32>>, n: i32) -> Vec<i32> {
+        let mut diffs = vec![0; n as usize + 1];
+        for b in bookings {
+            diffs[b[0] as usize - 1] += b[2];
+            diffs[b[1] as usize] -= b[2];
+        }
+
+        let mut v = Vec::new();
+        let mut ps = 0;
+        for i in 0..n as usize {
+            ps += diffs[i];
+            v.push(ps);
+        }
+
+        v
+    }
+}
