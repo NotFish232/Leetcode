70daecc060b4258ab8610278710a8464b3350181
Justin Lee <justin.adam.lee@gmail.com>
2025-03-14 21:26:44 -0400

    daily

---
+++ src/bin/2560.house-robber-iv.rs
@@ -0,0 +1,28 @@
+impl Solution {
+    pub fn min_capability(nums: Vec<i32>, k: i32) -> i32 {
+        let (mut l, mut r) = (1, *nums.iter().max().unwrap());
+
+        while l < r {
+            let mid = l + (r - l) / 2;
+
+            let mut count = 0;
+
+            let mut i = 0;
+            while i < nums.len() {
+                if nums[i] <= mid {
+                    count += 1;
+                    i += 1;
+                }
+                i += 1;
+            }
+
+            if count >= k {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        l
+    }
+}
