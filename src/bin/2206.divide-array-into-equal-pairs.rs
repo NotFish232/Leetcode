82ce358bdd9be9cc9457655b98cf29d1f2aed0ba
Justin Lee <justin.adam.lee@gmail.com>
2025-03-17 00:51:13 -0400

    crazy easy daily

---
+++ src/bin/2206.divide-array-into-equal-pairs.rs
@@ -0,0 +1,17 @@
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn divide_array(nums: Vec<i32>) -> bool {
+        let mut h = HashSet::new();
+
+        for n in nums {
+            if h.contains(&n) {
+                h.remove(&n);
+            } else {
+                h.insert(n);
+            }
+        }
+
+        h.is_empty()
+    }
+}
