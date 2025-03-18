dbe3a83f967c48311e3aed32e03c0de501d7f229
Justin Lee <justin.adam.lee@gmail.com>
2025-03-18 13:21:20 -0400

    daily

---
+++ src/bin/2401.longest-nice-subarray.rs
@@ -0,0 +1,24 @@
+use std::cmp::max;
+
+impl Solution {
+    pub fn longest_nice_subarray(nums: Vec<i32>) -> i32 {
+        let (mut l, mut r) = (0, 1);
+        let mut c = nums[0];
+        let mut max_count = 1;
+
+        while r < nums.len() {
+            while c & nums[r] != 0 {
+                c ^= nums[l];
+                l += 1;
+            }
+
+            c |= nums[r];
+
+            max_count = max(max_count, (r - l + 1) as i32);
+
+            r += 1;
+        }
+
+        max_count
+    }
+}
