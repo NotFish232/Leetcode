c8866b27a9ccbac14c763601d02bf8b4b67d01d2
Justin Lee <justin.adam.lee@gmail.com>
2025-03-18 14:38:22 -0400

    1004

---
+++ src/bin/1004.max-consecutive-ones-iii.rs
@@ -0,0 +1,29 @@
+use std::cmp::max;
+
+impl Solution {
+    pub fn longest_ones(nums: Vec<i32>, k: i32) -> i32 {
+        let (mut l, mut r) = (0, 0);
+        let mut zero_count = 0;
+        let mut max_count = 0;
+
+        while r < nums.len() {
+            if nums[r] == 0 {
+                zero_count += 1;
+            }
+
+            if zero_count > k {
+                zero_count -= 1;
+                while nums[l] != 0 {
+                    l += 1;
+                }
+                l += 1;
+            }
+
+            max_count = max(max_count, (r - l + 1) as i32);
+
+            r += 1;
+        }
+
+        max_count
+    }
+}
