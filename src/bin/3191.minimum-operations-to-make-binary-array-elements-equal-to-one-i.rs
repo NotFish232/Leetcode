5422116d9d425b5bf4cdf69620f0c12ab4fa0f3a
Justin Lee <justin.adam.lee@gmail.com>
2025-03-18 22:47:32 -0400

    slightly better

--- src/bin/3191.minimum-operations-to-make-binary-array-elements-equal-to-one-i.rs
+++ src/bin/3191.minimum-operations-to-make-binary-array-elements-equal-to-one-i.rs
@@ -2,8 +2,12 @@ impl Solution {
     pub fn min_operations(mut nums: Vec<i32>) -> i32 {
         let mut count = 0;
 
-        for i in 0..nums.len() - 2 {
+        for i in 0..nums.len() {
             if nums[i] == 0 {
+                if i + 2 >= nums.len() {
+                    return -1;
+                }
+
                 nums[i + 1] ^= 1;
                 nums[i + 2] ^= 1;
 
@@ -11,10 +15,6 @@ impl Solution {
             }
         }
 
-        if nums[nums.len() - 1] == 1 && nums[nums.len() - 2] == 1 {
-            count
-        } else {
-            -1
-        }
+        count
     }
 }
