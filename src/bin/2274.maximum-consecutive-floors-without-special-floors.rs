8401cda310d8d67d90119a4eed3f6a915b18c7aa
Justin Lee <justin.adam.lee@gmail.com>
2025-03-21 10:10:20 -0400

    ez

---
+++ src/bin/2274.maximum-consecutive-floors-without-special-floors.rs
@@ -0,0 +1,17 @@
+use std::cmp::max;
+
+impl Solution {
+    pub fn max_consecutive(bottom: i32, top: i32, mut special: Vec<i32>) -> i32 {
+        special.sort();
+        special.insert(0, bottom - 1);
+        special.push(top + 1);
+
+        let mut max_count = 0;
+
+        for i in 0..special.len() - 1 {
+            max_count = max(max_count, special[i + 1] - special[i] - 1);
+        }
+
+        max_count
+    }
+}
