d9b64007185176953002eef70cbbcefdf0445351
Justin Lee <justin.adam.lee@gmail.com>
2025-03-23 20:30:19 -0400

    daily

---
+++ src/bin/3169.count-days-without-meetings.rs
@@ -0,0 +1,29 @@
+use std::{cmp::Reverse, collections::BinaryHeap};
+
+impl Solution {
+    pub fn count_days(days: i32, meetings: Vec<Vec<i32>>) -> i32 {
+        let mut h = BinaryHeap::new();
+
+        for m in meetings {
+            h.push((Reverse(m[0]), 1));
+            h.push((Reverse(m[1]), -1))
+        }
+        h.push((Reverse(days + 1), 1));
+
+        let mut count = 0;
+
+        let mut d = 0;
+        let mut c = 0;
+
+        while let Some(((Reverse(t)), b)) = h.pop() {
+            if c == 0 {
+                count += (t - d - 1);
+            }
+
+            c += b;
+            d = t;
+        }
+
+        count
+    }
+}
