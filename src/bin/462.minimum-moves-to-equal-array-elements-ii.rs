1bb3524e357bae5076e605b0bf121b11f58a2f87
Justin Lee <justin.adam.lee@gmail.com>
2025-03-25 21:10:02 -0400

    legit same as daily

---
+++ src/bin/462.minimum-moves-to-equal-array-elements-ii.rs
@@ -0,0 +1,9 @@
+impl Solution {
+    pub fn min_moves2(mut nums: Vec<i32>) -> i32 {
+        nums.sort();
+
+        (0..nums.len())
+            .map(|i| (nums[i] - nums[nums.len() / 2]).abs())
+            .sum()
+    }
+}
