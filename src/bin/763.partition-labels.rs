e5cc80587747c2d9bd17195a851bdf7c48095fd9
Justin Lee <justin.adam.lee@gmail.com>
2025-03-30 18:02:29 -0400

    daily

---
+++ src/bin/763.partition-labels.rs
@@ -0,0 +1,38 @@
+use std::{cmp::max, collections::HashMap};
+
+impl Solution {
+    pub fn partition_labels(s: String) -> Vec<i32> {
+        let mut first_occur = HashMap::new();
+        let mut last_occur = HashMap::new();
+
+        for (i, ch) in s.chars().enumerate() {
+            if !first_occur.contains_key(&ch) {
+                first_occur.insert(ch, i);
+            }
+            last_occur.insert(ch, i);
+        }
+
+        let mut intervals: Vec<_> = first_occur
+            .iter()
+            .map(|(&k, &v)| (v, *last_occur.get(&k).unwrap()))
+            .collect();
+
+        intervals.sort();
+
+        let mut new_intervals: Vec<(usize, usize)> = Vec::new();
+
+        for (a, b) in intervals {
+            let idx = new_intervals.len() - 1;
+            if new_intervals.is_empty() || new_intervals[idx].1 < a {
+                new_intervals.push((a, b));
+            } else {
+                new_intervals[idx].1 = max(new_intervals[idx].1, b);
+            }
+        }
+
+        new_intervals
+            .iter()
+            .map(|&(a, b)| (b - a + 1) as i32)
+            .collect()
+    }
+}
