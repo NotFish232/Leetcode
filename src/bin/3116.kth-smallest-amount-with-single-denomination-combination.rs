59b56766db04659df0ab19ccc7f9480ab6d7404a
Justin Lee <justin.adam.lee@gmail.com>
2025-04-02 18:17:15 -0400

    a lot faster

--- src/bin/3116.kth-smallest-amount-with-single-denomination-combination.rs
+++ src/bin/3116.kth-smallest-amount-with-single-denomination-combination.rs
@@ -19,8 +19,18 @@ impl Solution {
         res
     }
 
-    fn count_num(amt: i64, coins: &[i64]) -> i64 {
+    fn count_num(amt: i64, lcms: &[(i64, i64)]) -> i64 {
         let mut count = 0;
+
+        for (lcm, sign) in lcms {
+            count += sign * amt / lcm;
+        }
+
+        count
+    }
+
+    fn make_lcms(coins: &[i64]) -> Vec<(i64, i64)> {
+        let mut v = Vec::new();
 
         for i in 1..=coins.len() {
             for combo in coins.iter().combinations(i) {
@@ -28,22 +38,23 @@ impl Solution {
 
                 let lcm = Self::lcm_n(&combo);
 
-                count += sign * amt / lcm;
+                v.push((lcm, sign));
             }
         }
 
-        count
+        v
     }
 
     pub fn find_kth_smallest(coins: Vec<i32>, k: i32) -> i64 {
         let coins: Vec<_> = coins.into_iter().map(|c| c as i64).collect();
+        let lcms = Self::make_lcms(&coins);
 
         let (mut l, mut r) = (0, i64::MAX);
 
         while l < r {
             let m = l + (r - l) / 2;
 
-            if Self::count_num(m, &coins) >= k as i64 {
+            if Self::count_num(m, &lcms) >= k as i64 {
                 r = m
             } else {
                 l = m + 1;
