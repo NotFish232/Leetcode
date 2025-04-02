5163c6fbd07b8a7f32ad29a236eeac4cb3e841c9
Justin Lee <justin.adam.lee@gmail.com>
2025-04-01 20:13:09 -0400

    constraint of 100 is crazy

---
+++ src/bin/2873.maximum-value-of-an-ordered-triplet-i.rs
@@ -0,0 +1,19 @@
+use std::cmp::max;
+
+impl Solution {
+    pub fn maximum_triplet_value(nums: Vec<i32>) -> i64 {
+        let mut max_val = 0;
+
+        let nums: Vec<_> = nums.into_iter().map(|x| x as i64).collect();
+
+        for i in 0..nums.len() {
+            for j in i + 1..nums.len() {
+                for k in j + 1..nums.len() {
+                    max_val = max(max_val, (nums[i] - nums[j]) * nums[k]);
+                }
+            }
+        }
+
+        max_val
+    }
+}
