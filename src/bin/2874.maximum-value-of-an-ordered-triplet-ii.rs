f2b78f4b56da031cc327564a64a32b5c04364b18
Justin Lee <justin.adam.lee@gmail.com>
2025-04-02 20:47:07 -0400

    repeat

---
+++ src/bin/2874.maximum-value-of-an-ordered-triplet-ii.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn maximum_triplet_value(nums: Vec<i32>) -> i64 {
+        let mut i = 0;
+        let mut dij = 0;
+        let mut res = 0;
+
+        for n in nums {
+            res = res.max(dij as i64 * n as i64);
+            i = i.max(n);
+            dij = dij.max(i - n);
+        }
+
+        res
+    }
+}
