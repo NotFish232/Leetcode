45509f15a7e9e87e941f7c4ce4316abc5a656a00
Justin Lee <justin.adam.lee@gmail.com>
2025-04-16 20:40:17 -0400

    better 1 liner?

--- src/solutions/2176_count-equal-and-divisible-pairs-in-an-array.rs
+++ src/solutions/2176_count-equal-and-divisible-pairs-in-an-array.rs
@@ -7,11 +7,8 @@ struct Solution;
 impl Solution {
     pub fn count_pairs(nums: Vec<i32>, k: i32) -> i32 {
         (0..nums.len())
-            .map(|i| {
-                (i + 1..nums.len())
-                    .map(|j| (nums[i] == nums[j] && (i * j) as i32 % k == 0) as i32)
-                    .sum::<i32>()
-            })
+            .flat_map(|i| (i + 1..nums.len()).map(move |j| (i, j)))
+            .map(|(i, j)| (nums[i] == nums[j] && (i * j) as i32 % k == 0) as i32)
             .sum()
     }
 }
