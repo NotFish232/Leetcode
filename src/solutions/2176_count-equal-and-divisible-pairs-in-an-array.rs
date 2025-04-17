37a82b56b0fc4e4b01ebdec8dbb52381fcec20b9
Justin Lee <justin.adam.lee@gmail.com>
2025-04-16 20:42:00 -0400

    ok

--- src/solutions/2176_count-equal-and-divisible-pairs-in-an-array.rs
+++ src/solutions/2176_count-equal-and-divisible-pairs-in-an-array.rs
@@ -8,8 +8,8 @@ impl Solution {
     pub fn count_pairs(nums: Vec<i32>, k: i32) -> i32 {
         (0..nums.len())
             .flat_map(|i| (i + 1..nums.len()).map(move |j| (i, j)))
-            .map(|(i, j)| (nums[i] == nums[j] && (i * j) as i32 % k == 0) as i32)
-            .sum()
+            .filter(|&(i, j)| nums[i] == nums[j] && (i * j) as i32 % k == 0)
+            .count() as i32
     }
 }
 // end_submission
