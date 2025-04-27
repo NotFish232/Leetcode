82b0d3465f0292a6b9c12b9e456552b9a5de3231
Justin Lee <justin.adam.lee@gmail.com>
2025-04-26 20:14:20 -0400

    one line

--- src/solutions/3392_count-subarrays-of-length-three-with-a-condition.rs
+++ src/solutions/3392_count-subarrays-of-length-three-with-a-condition.rs
@@ -6,13 +6,9 @@ struct Solution;
 // start_submission
 impl Solution {
     pub fn count_subarrays(nums: Vec<i32>) -> i32 {
-        let mut count = 0;
-
-        for i in 0..nums.len() - 2 {
-            count += (2 * (nums[i] + nums[i + 2]) == nums[i + 1]) as i32;
-        }
-
-        count
+        (0..nums.len() - 2)
+            .filter(|&i| 2 * (nums[i] + nums[i + 2]) == nums[i + 1])
+            .count() as i32
     }
 }
 // end_submission
