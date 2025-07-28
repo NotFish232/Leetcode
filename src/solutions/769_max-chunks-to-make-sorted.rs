3ea979a04deb3af24f4a782130c23816a5044094
Justin Lee <justin.adam.lee@gmail.com>
2025-07-28 10:02:37 -0400

    better sol

--- src/solutions/769_max-chunks-to-make-sorted.rs
+++ src/solutions/769_max-chunks-to-make-sorted.rs
@@ -4,18 +4,17 @@ use crate::stubs::*;
 struct Solution;
 
 // start_submission
+use std::cmp::max;
+
 impl Solution {
     pub fn max_chunks_to_sorted(arr: Vec<i32>) -> i32 {
-        let mut starting_i = 0;
-        let mut mask = 0;
+        let mut cur_max = -1;
         let mut count = 0;
 
         for (i, x) in arr.into_iter().enumerate() {
-            mask |= 1 << x;
+            cur_max = max(cur_max, x);
 
-            if mask == 2i32.pow((i + 1) as u32) - 2i32.pow(starting_i as u32) {
-                mask = 0;
-                starting_i = i + 1;
+            if i as i32 == cur_max {
                 count += 1;
             }
         }
