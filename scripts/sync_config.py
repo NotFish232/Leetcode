46068287d6fa87be662c92077e0aaecba6a085e8
Justin Lee <justin.adam.lee@gmail.com>
2025-08-28 15:42:47 -0400

    sync config

--- scripts/sync_config.py
+++ scripts/sync_config.py
@@ -11,11 +11,12 @@ CSRFTOKEN_KEY = "csrftoken"
 LEETCODE_SESSION_KEY = "LEETCODE_SESSION"
 
 
-CONFIG_LOCATION = "~/.leetcode/leetcode.toml"
-CONFIG_LOCATION = os.path.expanduser(CONFIG_LOCATION)
+INITIAL_CONFIG_LOCATION = "./cli_config.toml"
+TARGET_CONFIG_LOCATION = "~/.leetcode/leetcode.toml"
+TARGET_CONFIG_LOCATION = os.path.expanduser(TARGET_CONFIG_LOCATION)
 
-CSRF_CONFIG_REGEX = re.compile(r"^csrf = \".*\"$", re.MULTILINE)
-SESSION_CONFIG_REGEX = re.compile(r"^session = \".*\"$", re.MULTILINE)
+CSRF_CONFIG_REGEX = re.compile(r"^csrf = \"<csrftoken>\"$", re.MULTILINE)
+SESSION_CONFIG_REGEX = re.compile(r"^session = \"<LEETCODE_SESSION>\"$", re.MULTILINE)
 
 
 def main() -> None:
@@ -25,13 +26,13 @@ def main() -> None:
     csrftoken = cookies[CSRFTOKEN_KEY]
     leetcode_session = cookies[LEETCODE_SESSION_KEY]
 
-    with open(CONFIG_LOCATION, "r") as f:
+    with open(INITIAL_CONFIG_LOCATION, "r") as f:
         content = f.read()
 
     content = CSRF_CONFIG_REGEX.sub(f'csrf = "{csrftoken}"', content)
     content = SESSION_CONFIG_REGEX.sub(f'session = "{leetcode_session}"', content)
 
-    with open(CONFIG_LOCATION, "w") as f:
+    with open(TARGET_CONFIG_LOCATION, "w") as f:
         f.write(content)
 
 
