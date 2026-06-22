1cf1c4bc16256d85e0fbf4363430838d54b89596
NotFish232 <justin.adam.lee@gmail.com>
2026-06-22 19:30:14 -0400

    one Q

---
+++ src/cpp_solutions/1673_find-the-most-competitive-subsequence.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// start_submission
+#include <queue>
+
+class Solution {
+public:
+    vector<int> mostCompetitive(vector<int>& nums, int k) {
+        vector<int> stk;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            while (stk.size() > 0 && nums[i] < stk.back() && stk.size() + nums.size() - i - 1 >= k) {
+                stk.pop_back();
+            }
+
+            stk.push_back(nums[i]);
+        }
+
+        while (stk.size() > k) {
+            stk.pop_back();
+        }
+
+        return stk;
+    }
+};
+// end_submission
